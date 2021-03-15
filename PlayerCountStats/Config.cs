54205b40b266707e887428a31aab18b01f17a373
Evitonative <47825249+Evitonative@users.noreply.github.com>
2021-03-15 18:08:07 +0100

    Update Config.cs

--- PlayerCountStats/Config.cs
+++ PlayerCountStats/Config.cs
@@ -6,7 +6,7 @@ namespace PlayerCountStats
     public class Config : IConfig
     {
         public bool IsEnabled { get; set; } = false;
-        [Description("Path: WIN: C:\\Users\\USER\\AppData\\Roaming\\EXILED\\playerCount.csv | Linux: /home/USER/EXILED/playerCount.csv")]
+        [Description("Path: WIN: C:\\Users\\USER\\AppData\\Roaming\\EXILED\\playerCount.csv | Linux: /home/USER/.config/EXILED/playerCount.csv")]
         public string fileName { get; set; } = "";
     }
 }
