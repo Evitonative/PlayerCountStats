88e14e357115feb3b384a8a103ec36b14ec125bb
Evitonative <47825249+Evitonative@users.noreply.github.com>
2021-03-15 17:56:03 +0100

    Add project files.

---
+++ PlayerCountStats/Config.cs
@@ -0,0 +1,12 @@
+﻿using System.ComponentModel;
+using Exiled.API.Interfaces;
+
+namespace PlayerCountStats
+{
+    public class Config : IConfig
+    {
+        public bool IsEnabled { get; set; } = false;
+        [Description("Path: WIN: C:\\Users\\USER\\AppData\\Roaming\\EXILED\\playerCount.csv | Linux: /home/USER/EXILED/playerCount.csv")]
+        public string fileName { get; set; } = "";
+    }
+}
---
+++ PlayerCountStats/Plugin.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+using Exiled.API.Features;
+using Exiled.Events.EventArgs;
+
+namespace PlayerCountStats
+{
+    public class Plugin : Plugin<Config>
+    {
+        private static Config config = new Config();
+        public override void OnEnabled()
+        {
+            Exiled.Events.Handlers.Player.Joined += Join;
+            Exiled.Events.Handlers.Player.Destroying += Destroy;
+            Exiled.Events.Handlers.Server.WaitingForPlayers += Waiting;
+        }
+
+        public override void OnDisabled()
+        {
+            Exiled.Events.Handlers.Player.Joined -= Join;
+            Exiled.Events.Handlers.Player.Destroying -= Destroy;
+            Exiled.Events.Handlers.Server.WaitingForPlayers -= Waiting;
+        }
+
+        private void Waiting()
+        {
+            TryCreateFile();
+        }
+
+        private void Destroy(DestroyingEventArgs ev)
+        {
+            AppendedCountLine(Exiled.API.Features.Player.List.Count() - 1);
+        }
+
+        private void Join(JoinedEventArgs ev)
+        {
+            AppendedCountLine(Exiled.API.Features.Player.List.Count() + 1);
+        }
+
+        private static void AppendedCountLine(int count)
+        {
+            string line =
+                $"\"{DateTime.Now.ToString(System.Globalization.CultureInfo.CurrentCulture)}\";\"{count}\"";
+            File.AppendAllLines(config.fileName, new []{ line });
+        }
+
+        private static void TryCreateFile()
+        {
+            if (!File.Exists(config.fileName))
+            {
+                Directory.CreateDirectory(Path.GetDirectoryName(config.fileName) ?? string.Empty);
+                File.AppendAllLines(config.fileName, new[] { "Time;Count" });
+                Log.Info("File was missing. Creating: " + config.fileName);
+            }
+        }
+    }
+}
